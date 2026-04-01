68e075cb1407449c06b5afd42e4c665ea2434fc4
Mathias (EndeavourOS) <mathiasflansmose@gmail.com>
2026-04-01 22:00:26 +0200

    Added robots.txt

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from datetime import datetime
 from course_cards import COURSE_CARDS
+
 
 app = Flask(__name__)
 
@@ -10,6 +11,12 @@ def hello_world():
     return render_template("index.html", course_cards=COURSE_CARDS, current_year=datetime.now().year)
 
 
+@app.route('/robots.txt')
+def static_from_root():
+    return send_from_directory(app.static_folder, 'robots.txt')
+
+
 if __name__ == '__main__':
     # Flaks listen on all network: host='0.0.0.0'
     app.run(host='0.0.0.0', port=5000)
+
